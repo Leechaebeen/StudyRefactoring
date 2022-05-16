dc6001cc90c3f5cecad949e9b845f062cc9cddaa
lee chaebeen <chaebeen@mailplug.co.kr>
2022-05-17 07:20:58 +0900

    리팩토링 15. 플래그 인수 제거하기

--- src/main/java/me/whiteship/refactoring/_04_long_parameter_list/_15_remove_flag_argument/Order.java
+++ src/main/java/me/whiteship/refactoring/_04_long_parameter_list/_15_remove_flag_argument/Order.java
@@ -2,6 +2,14 @@ package me.whiteship.refactoring._04_long_parameter_list._15_remove_flag_argumen
 
 import java.time.LocalDate;
 
+/**
+ * 리팩토링 15. 플래그 인수 제거하기 Remove Flag Argument
+ * - 플래그는 보통 함수에 매개변수로 전달해서, 함수 내부의 로직을 분기하는데 사용한다.
+ * - 플래그를 사용한 함수는 차이를 파악하기 힘들다.
+ *      - bookConcert(customer, false), bookConcert(customer, true)
+ *      - bookConcert(customer), premiumBookConcert(customer)
+ * - 조건문 분해하기(Decompose Condition)를 활용할 수 있다.
+ * */
 public class Order {
 
     private LocalDate placedOn;
