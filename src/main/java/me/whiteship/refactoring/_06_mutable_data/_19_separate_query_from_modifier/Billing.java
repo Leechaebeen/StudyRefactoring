e01da6c811fa94e32a1c9557abc6ca9a4ae79907
lee chaebeen <chaebeen@mailplug.co.kr>
2022-05-20 07:37:49 +0900

    리팩토링 19. 질의함수와 변경 함수 분리하기

--- src/main/java/me/whiteship/refactoring/_06_mutable_data/_19_separate_query_from_modifier/Billing.java
+++ src/main/java/me/whiteship/refactoring/_06_mutable_data/_19_separate_query_from_modifier/Billing.java
@@ -1,5 +1,14 @@
 package me.whiteship.refactoring._06_mutable_data._19_separate_query_from_modifier;
 
+/** 리팩토링 19. 질의 함수와 변경 함수 분리하기 Separate Query from Modifier
+ * - "눈에 띌만한" 사이드 이펙트 없이 값을 조회할 수 있는 메소드는 테스트하기도 쉽고, 메소드를 이동하기도 편하다.
+ * - 명령-조회 분리(command-query separate) 규칙
+ *      - 어떤 값을 리턴하는 함수는 사이드 이펙트가 없어야 한다.
+ * - "눈에 띌만한 (observable) 사이드 이펙트"
+ *      - 가령, 캐시는 중요한 객체 상태 변화는 아니다.
+ *        따라서 어떤 메소드 호출로 인해, 캐시 데이터를 변경하더라도 분리할 필요는 없다.
+ * */
+// getter setter 처럼 구분하기
 public class Billing {
 
     private Customer customer;
@@ -11,15 +20,13 @@ public class Billing {
         this.emailGateway = emailGateway;
     }
 
-    public double getTotalOutstandingAndSendBill() {
-        double result = customer.getInvoices().stream()
+    public double getTotalOutstanding() {
+        return customer.getInvoices().stream()
                 .map(Invoice::getAmount)
                 .reduce((double) 0, Double::sum);
-        sendBill();
-        return result;
     }
 
-    private void sendBill() {
+    public void sendBill() {
         emailGateway.send(formatBill(customer));
     }
 
