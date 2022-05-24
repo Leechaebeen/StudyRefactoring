2f5c5f13773a281d02507706947463ead29f8538
lee chaebeen <chaebeen@mailplug.co.kr>
2022-05-25 08:03:49 +0900

    리팩토링 25.함수 옮기기

--- src/main/java/me/whiteship/refactoring/_07_divergent_change/_25_move_function/Account.java
+++ src/main/java/me/whiteship/refactoring/_07_divergent_change/_25_move_function/Account.java
@@ -1,8 +1,21 @@
 package me.whiteship.refactoring._07_divergent_change._25_move_function;
 
+/**
+ * 리팩토링 25.함수 옮기기 Move Function
+ * - 모듈화가 잘 된 소프트웨어는 최소한의 지식만으로 프로그램을 변경할 수 있다.
+ * - 관련있는 함수나 필드가 모여있어야 더 쉽게 찾고 이해할 수 있다.
+ * - 하지만 관련있는 함수나 필드가 항상 고정적인 것은 아니기 때문에 때에 따라 옮겨야 할 필요가 있다.
+ * - 함수를 옮겨야 하는 경우
+ *      - 해당 함수가 다른 문맥(클래스)에 있는 데이터(필드)를 더 많이 참조하는 경우.
+ *      - 해당 함수를 다른 클라이언트(클래스)에서도 필요로 하는 경우.
+ * - 함수를 옮겨갈 새로운 문캑(클래스)이 필요한 경우에는 "여러 함수를 클래스로 묶기(Combine Functions info Class)"
+ *   또는 "클래스 추출하기(Extract Class)" 를 사용한다.
+ * - 함수를 옮길 적당한 위치를 찾기가 어렵다면 그대로 두어도 괜찮다. 언제든 나중에 옮길 수 있다.
+ *
+ * */
 public class Account {
 
-    private int daysOverdrawn;
+    int daysOverdrawn;
 
     private AccountType type;
 
@@ -14,7 +27,7 @@ public class Account {
     public double getBankCharge() {
         double result = 4.5;
         if (this.daysOverdrawn() > 0) {
-            result += this.overdraftCharge();
+            result += this.type.overdraftCharge(this.daysOverdrawn);
         }
         return result;
     }
@@ -23,16 +36,4 @@ public class Account {
         return this.daysOverdrawn;
     }
 
-    private double overdraftCharge() {
-        if (this.type.isPremium()) {
-            final int baseCharge = 10;
-            if (this.daysOverdrawn <= 7) {
-                return baseCharge;
-            } else {
-                return baseCharge + (this.daysOverdrawn - 7) * 0.85;
-            }
-        } else {
-            return this.daysOverdrawn * 1.75;
-        }
-    }
 }
--- src/main/java/me/whiteship/refactoring/_07_divergent_change/_25_move_function/AccountType.java
+++ src/main/java/me/whiteship/refactoring/_07_divergent_change/_25_move_function/AccountType.java
@@ -3,11 +3,22 @@ package me.whiteship.refactoring._07_divergent_change._25_move_function;
 public class AccountType {
     private boolean premium;
 
-    public AccountType(boolean premium) {
-        this.premium = premium;
-    }
-
     public boolean isPremium() {
         return this.premium;
+    }
+
+    // 예제에서는 Account 를 넘겼지만 account.xxx 이런 식으로 참조하는 값이 많다면
+    // Account 클래스에 overdraftCharge(int daysOverdrawn) 메소드를 두는 것이 더 적절하다고 볼 수 있다.
+    double overdraftCharge(int daysOverdrawn) {
+        if (this.isPremium()) {
+            final int baseCharge = 10;
+            if (daysOverdrawn <= 7) {
+                return baseCharge;
+            } else {
+                return baseCharge + (daysOverdrawn - 7) * 0.85;
+            }
+        } else {
+            return daysOverdrawn * 1.75;
+        }
     }
 }
