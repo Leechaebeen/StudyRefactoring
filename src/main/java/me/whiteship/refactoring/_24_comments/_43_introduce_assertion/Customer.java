189c73359ca76de619e2e028451f1db749ba9580
lee chaebeen <chaebeen@mailplug.co.kr>
2022-07-18 21:32:38 +0900

    리팩토링43.어서션 추가하기

--- src/main/java/me/whiteship/refactoring/_24_comments/_43_introduce_assertion/Customer.java
+++ src/main/java/me/whiteship/refactoring/_24_comments/_43_introduce_assertion/Customer.java
@@ -10,6 +10,16 @@ package me.whiteship.refactoring._24_comments._43_introduce_assertion;
  *      - "함수 선언부 변경하기"를 사용해 함수 이름을 재정의할 수 있다.
  *      - 시스템적으로 어떤 필요한 규칙이 있다면, "어셔션 추가하기(Introduce Assertion)" 을 적용할 수 있다.
  * */
+
+/**
+ * 리팩토링 43. 어서션 추가하기 Introduce Assertion
+ * - 종종 코드로 표현하지 않았지만 기본적으로 가정하고 있는 조건들이 있다. 그런 조건을 알고리즘을 파악하거나 주석을 읽으면서 확인할 수 있다,
+ * - 그러한 조건을 Assertion 을 사용해서 보다 명시적으로 나타낼 수 있다.
+ * - Assertion 은 if 나 switch 문과 달리 "항상" true 이길 기대하는 조건을 표현할 때 사용한다.
+ *      - 프로그램이 Assertion 에서 실패한다면 프로그래머의 실수로 생각할 수 있다.
+ *      - Assertion이 없어도 프로그램이 동작해야 한다.(자바에서는 컴파일 옵션으로 assert 문을 사용하지 않도록 설정할 수도 있다.)
+ * - 특정 부분에선 특정한 상태를 가정하고 있다는 것을 명시적으로 나타냄으로써, 의사소통적인 가치를 지니고 있다.
+ * */
 public class Customer {
 
     private Double discountRate;
@@ -23,6 +33,13 @@ public class Customer {
     }
 
     public void setDiscountRate(Double discountRate) {
+        // 의사소통
+        assert discountRate != null && discountRate > 0;
+
+        // 코드 상 제어
+        if(discountRate == null && discountRate < 0) {
+            throw new IllegalArgumentException(discountRate + " can't be minus.");
+        }
         this.discountRate = discountRate;
     }
 }
